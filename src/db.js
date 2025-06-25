586697d5ffa52fefeacfbf9db65b7587424ae5bb
José Isaac <joseisaacnascimento@gmail.com>
2025-06-25 16:40:31 -0300

    feat: Cria arquivo de db para a criação da tabela de projetos

---
+++ src/db.js
@@ -0,0 +1,24 @@
+// IMPORTS
+import sqlite3 from "sqlite3";
+
+// CONTANTES
+const { verbose } = sqlite3;
+const sqlite = verbose();
+const db = new sqlite.Database("database.db", (err) => {
+  if (err) {
+    console.error("Error opening database:", err.message);
+  } else {
+    console.log("Connected to the SQLite database.");
+  }
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS projetos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT NOT NULL,
+  descricao TEXT,
+  data_criacao TEXT DEFAULT (datetime('now')),
+  status TEXT
+)`);
+
+// EXPORTAÇÃO
+export default db;
